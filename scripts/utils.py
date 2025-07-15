69493abc381df92058748b03f7a13f744e64df19
basic-cc <155611043+basic-cc@users.noreply.github.com>
2025-07-15 09:43:41 -0400

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,6 @@
 import datetime
 
-def log_event(filename, event):
+def log_event(filename, event, extra=""):
     with open(filename, "a") as f:
         timestamp = datetime.datetime.now().isoformat()
-        f.write(f"{timestamp}, {event}\n")
+        f.write(f"{timestamp}, {event}, {extra}\n")
