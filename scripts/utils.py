57e0b874b2161de6be2f5555d125e26d2df73a4a
basic-cc <155611043+basic-cc@users.noreply.github.com>
2025-07-15 10:04:49 -0400

    Delete scripts/utils.py

--- scripts/utils.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-def log_event(filename, event, extra=""):
-    with open(filename, "a") as f:
-        timestamp = datetime.datetime.now().isoformat()
-        f.write(f"{timestamp}, {event}, {extra}\n")
